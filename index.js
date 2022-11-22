7ba70112941ce2bc5f3af45150bb84912eff33b2
moatasemEbrahim <moatasem@carry1st.com>
2022-11-23 00:57:57 +0200

    change moataz to moatasem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var app = express();
-app.get("/",function(request,response){response.send("Hello Motaz!")});
+app.get("/",function(request,response){response.send("Hello moatasem!")});
 app.listen(10000, function () {
     console.log("Started application on port %d", 10000)
 });
